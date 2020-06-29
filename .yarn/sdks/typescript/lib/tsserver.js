7be624b8ef848645a0b7e3350c664be69cde6047
clearbits <112537893+clearbits@users.noreply.github.com>
2020-06-29 07:16:16 +0000

    Updated the libprotobuf feedstock.

--- .yarn/sdks/typescript/lib/tsserver.js
+++ .yarn/sdks/typescript/lib/tsserver.js
@@ -3,3 +3,20 @@
 
 const {existsSync} = require(`fs`);
 const {createRequire} = require(`module`);
+const {resolve} = require(`path`);
+
+const relPnpApiPath = "../../../../.pnp.cjs";
+
+const absPnpApiPath = resolve(__dirname, relPnpApiPath);
+const absRequire = createRequire(absPnpApiPath);
+
+const moduleWrapper = tsserver => {
+  if (!process.versions.pnp) {
+    return tsserver;
+  }
+
+  const {isAbsolute} = require(`path`);
+  const pnpApi = require(`pnpapi`);
+
+  const isVirtual = str => str.match(/\/(\$\$virtual|__virtual__)\//);
+  const isPortal = str => str.startsWith("portal:/");
