54371094eebda177ef5167273192f9de0a426b7f
clearbits <112537893+clearbits@users.noreply.github.com>
2020-07-01 11:44:11 +0000

    Update dDocent

--- .yarn/sdks/typescript/lib/tsserver.js
+++ .yarn/sdks/typescript/lib/tsserver.js
@@ -68,3 +68,29 @@ const moduleWrapper = tsserver => {
           //
           // 2022-04-06: VSCode changed the format in 1.66.
           // Before | ^/zip//c:/foo/bar.zip/package.json
+          // After  | ^/zip/c:/foo/bar.zip/package.json
+          //
+          // 2022-05-06: VSCode changed the format in 1.68
+          // Before | ^/zip/c:/foo/bar.zip/package.json
+          // After  | ^/zip//c:/foo/bar.zip/package.json
+          //
+          case `vscode <1.61`: {
+            str = `^zip:${str}`;
+          } break;
+
+          case `vscode <1.66`: {
+            str = `^/zip/${str}`;
+          } break;
+
+          case `vscode <1.68`: {
+            str = `^/zip${str}`;
+          } break;
+
+          case `vscode`: {
+            str = `^/zip/${str}`;
+          } break;
+
+          // To make "go to definition" work,
+          // We have to resolve the actual file system path from virtual path
+          // and convert scheme to supported by [vim-rzip](https://github.com/lbrayner/vim-rzip)
+          case `coc-nvim`: {
