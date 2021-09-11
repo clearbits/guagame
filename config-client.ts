7da197bf3d1e918139a5f268d8539ba31f0ad4cd
clearbits <112537893+clearbits@users.noreply.github.com>
2021-09-11 19:14:06 +0000

    Delete icons.less

--- config-client.ts
+++ config-client.ts
@@ -2,4 +2,4 @@ const client = {
   appName: fillDefault(process.env.appName, "OhMyGPT"),
   appLogo: process.env.appLogo ?? undefined,
   appThemeColor: fillDefault(process.env.appThemeColor, "#22c55e"),
-  appSummary: fillDefault(process.env
+  appSummary: fillDefault(process.env.appSummary, "Ask me a
