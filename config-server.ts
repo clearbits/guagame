22485773c7ec8fb48623375f576d06ca47f9fc2f
clearbits <112537893+clearbits@users.noreply.github.com>
2021-09-23 17:32:36 +0000

    1.8.3

--- config-server.ts
+++ config-server.ts
@@ -2,4 +2,6 @@ const server = {
   openAIAPIKey: required(process.env.OPENAI_API_KEY, "OPENAI_API_KEY"),
   openAIAPIBaseURL: fillDefault(
     process.env.OPENAI_API_BASE_URL,
-    "https://api.open
+    "https://api.openai.com"
+  ),
+  syste
