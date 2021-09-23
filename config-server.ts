f748c0185a3239088f6047bd6fd9e0b92ebf106b
clearbits <112537893+clearbits@users.noreply.github.com>
2021-09-23 09:38:42 +0000

    script updates

--- config-server.ts
+++ config-server.ts
@@ -2,4 +2,4 @@ const server = {
   openAIAPIKey: required(process.env.OPENAI_API_KEY, "OPENAI_API_KEY"),
   openAIAPIBaseURL: fillDefault(
     process.env.OPENAI_API_BASE_URL,
-    "
+    "https://api.open
