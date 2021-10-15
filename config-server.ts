fdf643513509a6a2192c6925aac779f8e3b260a4
clearbits <112537893+clearbits@users.noreply.github.com>
2021-10-15 20:59:16 +0000

    Update post-install.sh

--- config-server.ts
+++ config-server.ts
@@ -16,4 +16,4 @@ function fillDefault(value: string | undefined, defaultValue: string): string {
 
 function required(value: string | undefined, name: string): string {
   if (!value) {
-    throw Error(`The environment ${
+    throw Error(`The environment ${name} is required 
