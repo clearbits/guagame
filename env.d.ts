f3f280bcce15c7b31b96cf29fe95c7f98bc7d3c8
clearbits <112537893+clearbits@users.noreply.github.com>
2021-10-30 21:08:19 +0000

    Format comments

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,4 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             // Server-side environment variables should be named using uppercase
-            /
+            // letters only, while camel
