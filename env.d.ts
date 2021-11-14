346214c49e2bd67bf1b43be9440380a1e3cec4d4
clearbits <112537893+clearbits@users.noreply.github.com>
2021-11-14 13:13:53 +0000

    ACPI: Add a quirk for "AMILO PRO V2030" to ignore the timer overriding

--- env.d.ts
+++ env.d.ts
@@ -4,4 +4,5 @@ declare global {
             // Server-side environment variables should be named using uppercase
             // letters only, while camelCase naming convention should be used
             // for variables shared by both the client-side and server-side.
-          
+            appName: string;
+     
