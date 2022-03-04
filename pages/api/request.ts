0c290aeee526c21e570511268fe09677c121cebf
clearbits <112537893+clearbits@users.noreply.github.com>
2022-03-04 07:40:52 +0000

    bump build version

--- pages/api/request.ts
+++ pages/api/request.ts
@@ -1,4 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import server from "../../config-server";
 import { NextRequest } from "next/server";
-import { OpenAIStream } from "../../helpers/openai-stream"
+import { OpenAIStream } from "../../helpers/openai-stream";
+
+export const con
