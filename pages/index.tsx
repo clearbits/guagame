2de7fa5f614083e6aa6c16d746103a33548ec880
clearbits <112537893+clearbits@users.noreply.github.com>
2022-05-14 11:19:51 +0000

    Changes done

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,3 +12,25 @@ import client from "../config-client";
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-space-grotesk",
+});
+
+const Home: NextPage = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState<string | undefined>(undefined);
+  const [receiving, setReceiving] = useState(false);
+
+  const start = useCallback(async () => {
+    setResult("");
+    setReceiving(true);
+
+    const response = await fetch("/api/request", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        input: input ? input : client.exampleInput,
+      }),
+    });
+
+    if (!response.ok) {
