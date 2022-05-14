8670c70d4c6fb5e1b43ef67aa44d78fd3d73d99c
clearbits <112537893+clearbits@users.noreply.github.com>
2022-05-14 17:56:25 +0000

    removing logs

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,3 +58,21 @@ const Home: NextPage = () => {
 
     setReceiving(false);
   }, [input]);
+
+  return (
+    <div className="relative flex min-h-screen overflow-hidden isolate flex-col items-center justify-start py-2 bg-gray-100 text-black dark:bg-neutral-900 dark:text-gray-100">
+      <Head>
+        <title>{client.appName}</title>
+        <link rel="icon" href={client.appLogo} />
+      </Head>
+      <BackgroundGradient className="top-0 left-0 h-96 w-48 bg-indigo-500/30 duration-500 dark:bg-blue-500/40" />
+      <BackgroundGradient className="left-60 top-96 h-64 w-72 rounded-lg bg-blue-500/30  duration-700 dark:bg-indigo-500/40" />
+      <BackgroundGradient className="right-96 bottom-60 h-60 w-60 rounded-lg bg-red-500/30 dark:bg-violet-500/30" />
+      <BackgroundGradient className="right-0 bottom-0 h-48 w-96 rounded-full bg-orange-500/30 dark:bg-cyan-500/30" />
+
+      <main className="flex w-full flex-1 flex-col items-center p-5 text-center">
+        {client.appLogo ? (
+          <img className="w-20 mt-20 h-20 rounded-2xl" src={client.appLogo} />
+        ) : undefined}
+        <h1
+          className={classNames(
